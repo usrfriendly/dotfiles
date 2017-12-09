e92deeeaa295ce87c7c819486c582205a910a906
Arin <arinlares@gmail.com>
2017-12-09 20:50:10 +0000

    removed xfce/ob keybinds, let session manager do that

--- openbox-xfce/transset.sh
+++ openbox-xfce/transset.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-trans="$(echo "Transparency" | dmenu -p "Transparency (1-100)")"
+trans="$(dmenu -p "Transparency (1-100)" -fn "Droid Sans Mono:10" -nf "#f9f9f9" -nb "#444444" -sb "#d64937" -sf "#f9f9f9")"
 
 compton-trans ${trans}
