c2849ee30860491ff8d68c21eb4e23e617ed9690
a-roc <arinlares@gmail.com>
2017-12-21 12:31:03 -0800

    back to OpenBox + Arch

--- openbox-xfce/transset.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-trans="$(dmenu -p "Transparency (1-100)" -fn "Droid Sans Mono:10" -nf "#f9f9f9" -nb "#444444" -sb "#d64937" -sf "#f9f9f9")"
-
-compton-trans ${trans}
--- openbox/transset.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-trans="$(zenity --entry --text='Transparency' --entry-text=100)"
-
-compton-trans ${trans}
