423e3ea539f79a1534a7b78de509bceacdca65fb
usrfriendly <usrfriendly@github.com>
2020-01-17 21:24:22 -0800

    Backing up current Arch i3

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-xautolock -corners "+-00" -time 1 -locker "/usr/bin/i3lock-fancy -p" -detectsleep
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
+xautolock -corners "+-00" -time 1 -locker "/usr/bin/i3lock -p default -i /tmp/screen.png" -detectsleep -cornerdelay 1
