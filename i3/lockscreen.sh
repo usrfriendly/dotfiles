24e22967c1d4dc9240df861e59e2bf7df2b94847
usrfriendly <usrfriendly@github.com>
2020-02-15 09:01:43 -0800

    Made a bunch of tweaks, screenlock, wallpaper

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -2,5 +2,5 @@
 
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-
-xautolock -corners "+-00" -time 1 -locker "/usr/bin/i3lock -p default -i /tmp/screen.png" -detectsleep -cornerdelay 1
+i3lock -i /tmp/screen.png
+rm /tmp/screen.png
