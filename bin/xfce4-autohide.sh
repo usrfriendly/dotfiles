038c0daca54005b3e0f808e15653647b00b786ae
Arin Lares <arin@x220.localdomain>
2020-05-10 21:05:34 -0700

    Fixed xfce4-autohide.sh

--- bin/xfce4-autohide.sh
+++ bin/xfce4-autohide.sh
@@ -2,11 +2,12 @@
 
 TEST="xfconf-query -c xfce4-panel -p /panels/panel-1/autohide-behavior"
 
-if [[ $TEST == 0 ]]
+if [[ `eval $TEST` == 0 ]]
 	then
 		xfconf-query -c xfce4-panel -p /panels/panel-1/autohide-behavior -s 1
-elif [[ $TEST == 1 ]] 
+elif [[ `eval $TEST` == 1 ]] 
 	then
+		echo $TEST
 		xfconf-query -c xfce4-panel -p /panels/panel-1/autohide-behavior -s 0
 else
 	xfconf-query -n -c xfce4-panel -p /panels/panel-1/autohide-behavior -s 1 -t int
