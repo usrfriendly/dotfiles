3fcfc0b06dedceb00d3628a62535db6da48894a8
usrfriendly <arinlares@gmail.com>
2022-02-16 01:32:06 -0800

    converting to Breeze theming + jwm, w/ kwallet backend

--- i3/wallpaper.sh
+++ i3/wallpaper.sh
@@ -3,4 +3,4 @@ SIZE=`xrandr --current | sed -n '2p' | awk '{print $4}' | cut -d "+" -f1`
 
 SCREENSIZE="$SIZE"
 
-feh --bg-fill /usr/share/wallpapers/Next/contents/images/$SIZE.png
+feh --bg-fill /usr/share/wallpapers/Next/contents/images/$SIZE.jpg
